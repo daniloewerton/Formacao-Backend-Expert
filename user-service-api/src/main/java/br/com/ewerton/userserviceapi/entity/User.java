e028dd5ee6f378a9b6786e2ffce086354503e2e5
Danilo Ewerton <danilo.ewe@gmail.com>
2024-10-12 10:32:31 -0300

    feat: add user entity

--- user-service-api/src/main/java/br/com/ewerton/userserviceapi/entity/User.java
+++ user-service-api/src/main/java/br/com/ewerton/userserviceapi/entity/User.java
@@ -1,11 +1,23 @@
 package br.com.ewerton.userserviceapi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
 import models.enums.ProfileEnum;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Set;
 
+@Data
+@With
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    @Id
     private String id;
     private String name;
     private String email;
