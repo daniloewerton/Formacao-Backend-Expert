12983d0834052ce41a0adeeb9606771d645a81fc
Danilo Ewerton <danilo.ewe@gmail.com>
2024-10-12 11:42:57 -0300

    feat: add endpoint find by id

---
+++ user-service-api/src/main/java/br/com/ewerton/userserviceapi/controller/impl/UserControllerImpl.java
@@ -0,0 +1,20 @@
+package br.com.ewerton.userserviceapi.controller.impl;
+
+import br.com.ewerton.userserviceapi.controller.UserController;
+import br.com.ewerton.userserviceapi.entity.User;
+import br.com.ewerton.userserviceapi.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserControllerImpl implements UserController {
+
+    private final UserService userService;
+
+    @Override
+    public ResponseEntity<User> findById(String id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+}
