9f66aa33eb35a9910132cc2935ebc5ad685e4c89
Danilo Ewerton <danilo.ewe@gmail.com>
2025-03-05 08:33:10 -0300

    feat: add validation to email already exists

--- user-service-api/src/main/java/br/com/ewerton/userserviceapi/repository/UserRepository.java
+++ user-service-api/src/main/java/br/com/ewerton/userserviceapi/repository/UserRepository.java
@@ -4,7 +4,9 @@ import br.com.ewerton.userserviceapi.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-
+    Optional<User> findByEmail(final String email);
 }
