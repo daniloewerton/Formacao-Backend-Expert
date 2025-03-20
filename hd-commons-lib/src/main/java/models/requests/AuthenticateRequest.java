aea58acdd38a2b38767fb4e12789ff4d664bbc04
Danilo Ewerton <danilo.ewe@gmail.com>
2025-03-20 08:37:25 -0300

    feat: add authenticate contract controller

---
+++ hd-commons-lib/src/main/java/models/requests/AuthenticateRequest.java
@@ -0,0 +1,25 @@
+package models.requests;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public record AuthenticateRequest(
+        @NotBlank(message = "Email cannot be empty")
+        @Email(message = "Invalid email")
+        @Schema(description = "User email", example = "danilo@gmail.com")
+        @Size(min = 6, max = 50, message = "Email must contain between 6 and 50 characters")
+        String email,
+
+        @NotBlank(message = "Password cannot be empty")
+        @Schema(description = "User password", example = "123456")
+        @Size(min = 6, max = 50, message = "Password must contain between 6 and 50 characters")
+        String password
+) implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
