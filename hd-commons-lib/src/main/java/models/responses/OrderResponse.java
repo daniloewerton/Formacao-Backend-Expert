f7f2b16eba135ba0e540bcfba67fd3840575e244
Danilo Ewerton <danilo.ewe@gmail.com>
2026-02-16 16:05:38 -0300

    feat: add order controller interface

---
+++ hd-commons-lib/src/main/java/models/responses/OrderResponse.java
@@ -0,0 +1,12 @@
+package models.responses;
+
+public record OrderResponse(
+        String id,
+        String requesterId,
+        String customerId,
+        String title,
+        String description,
+        String status,
+        String createdAt,
+        String closedAt
+) { }
---
+++ order-service-api/src/main/java/br/com/ewerton/orderserviceapi/controllers/OrderController.java
@@ -0,0 +1,31 @@
+package br.com.ewerton.orderserviceapi.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import models.requests.CreateOrderRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "OrderController", description = "Controller responsible for orders operations")
+@RequestMapping("/api/orders")
+public interface OrderController {
+
+    @Operation(summary = "Save new order")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+    })
+    @PostMapping
+    ResponseEntity<Void> save(@Valid @RequestBody final CreateOrderRequest request);
+}
