0fbda6da458a884e40c3e473fbf5bffc7f1d254a
Danilo Ewerton <danilo.ewe@gmail.com>
2026-02-16 15:51:38 -0300

    feat: add create order request structure

---
+++ hd-commons-lib/src/main/java/models/requests/CreateOrderRequest.java
@@ -0,0 +1,33 @@
+package models.requests;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateOrderRequest(
+
+        @Schema(description = "Requester ID", example = "698c8728c537bc0c968737b4")
+        @NotBlank(message = "The requesterId ID cannot be null or blank")
+        @Size(min = 24, max = 36, message = "The requesterId must be between 24 and 36 characters long")
+        String requesterId,
+
+        @Schema(description = "Customer ID", example = "698c8728c537bc0c968737b4")
+        @NotBlank(message = "The customerId ID cannot be null or blank")
+        @Size(min = 24, max = 36, message = "The customerId must be between 24 and 36 characters long")
+        String customerId,
+
+        @Schema(description = "Order title", example = "Fix my computer")
+        @NotBlank(message = "The title cannot be null or blank")
+        @Size(min = 3, max = 45, message = "The title must be between 3 and 45 characters long")
+        String title,
+
+        @Schema(description = "Order description", example = "My computer is broken")
+        @NotBlank(message = "The description cannot be null or blank")
+        @Size(min = 10, max = 3000, message = "The description must be between 10 and 3000 characters long")
+        String description,
+
+        @Schema(description = "Order status", example = "Open")
+        @NotBlank(message = "The status cannot be null or blank")
+        @Size(min = 4, max = 15, message = "The status must be between 4 and 15 characters long")
+        String status
+) { }
