d345e87a7a4417d8b0e0d5edf06aae3291b7ddc1
Danilo Ewerton <danilo.ewe@gmail.com>
2026-02-16 11:41:38 -0300

    feat: add database connection and create first structure

---
+++ order-service-api/src/main/java/br/com/ewerton/orderserviceapi/entities/Order.java
@@ -0,0 +1,40 @@
+package br.com.ewerton.orderserviceapi.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.now;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_order")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 45)
+    private String requestId;
+
+    @Column(nullable = false, length = 45)
+    private String customerId;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 3000)
+    private String description;
+
+    private final LocalDateTime createdAt = now();
+
+    private LocalDateTime closedAt;
+}
