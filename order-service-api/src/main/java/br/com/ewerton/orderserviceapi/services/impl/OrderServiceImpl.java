5fc5ccf208df2506d26a14216e1bad6e767f519c
Danilo Ewerton <danilo.ewe@gmail.com>
2026-02-17 10:51:55 -0300

    feat: add feign client and user service integration

--- order-service-api/src/main/java/br/com/ewerton/orderserviceapi/services/impl/OrderServiceImpl.java
+++ order-service-api/src/main/java/br/com/ewerton/orderserviceapi/services/impl/OrderServiceImpl.java
@@ -1,15 +1,18 @@
 package br.com.ewerton.orderserviceapi.services.impl;
 
+import br.com.ewerton.orderserviceapi.clients.UserServiceFeignClient;
 import br.com.ewerton.orderserviceapi.entities.Order;
 import br.com.ewerton.orderserviceapi.mapper.OrderMapper;
 import br.com.ewerton.orderserviceapi.repositories.OrderRepository;
 import br.com.ewerton.orderserviceapi.services.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import models.enums.OrderStatusEnum;
 import models.exceptions.ResourceNotFoundException;
 import models.requests.CreateOrderRequest;
 import models.requests.UpdateOrderRequest;
 import models.responses.OrderResponse;
+import models.responses.UserResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -19,12 +22,14 @@ import java.util.List;
 
 import static java.time.LocalDateTime.now;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository repository;
     private final OrderMapper mapper;
+    private final UserServiceFeignClient userServiceFeignClient;
 
     @Override
     public Order findById(final Long id) {
@@ -36,6 +41,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void save(CreateOrderRequest request) {
+        validateUserId(request.requesterId());
         repository.save(mapper.fromRequest(request));
     }
 
@@ -70,5 +76,10 @@ public class OrderServiceImpl implements OrderService {
         PageRequest pageRequest = PageRequest.of(
                 page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         return repository.findAll(pageRequest);
+    }
+
+    void validateUserId(final String userId) {
+        final UserResponse response = userServiceFeignClient.findById(userId).getBody();
+        log.info("User found: {}", response);
     }
 }
